fac9f640b32613b72f593be7ce8cef7711cb9b68
水波形 <suihakei@gmail.com>
2017-10-13 02:20:12 +0900

    DBに存在するユーザを扱うモデル

---
+++ include/model/DB/User.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * ユーザの情報を扱うDAO
+ */
+Class User extends Database
+{
+    /**
+     * データベースに接続するモデルを保存しておく変数
+     * @var Database
+     */
+    private $_dao;
+
+
+    /**
+     * コンストラクタ
+     */
+    public function __construct() {
+        # DAOをインスタンス化
+        $this->_dao = Database::getInstance();
+    }
+
+
+
+    /**
+     * メールアドレスとパスワードからユーザ情報を取得する
+     * （ログインのときとかに使う用）
+     *
+     * @param  string $email    メールアドレス
+     * @param  string $password パスワード
+     * @return mixed            ユーザ情報があればユーザの情報が配列で、なければfalseが返る
+     */
+    public function getLoginInfo(string $email, string $password) {
+        # DBからユーザ情報を検索するSQL
+        $sql = 'SELECT
+                    user_id, user_name, email, password, permission
+                FROM
+                    user
+                WHERE
+                    email = :email
+        ';
+
+        # プレースホルダーに当てはめる情報軍
+        $data = [
+            'email' => $email
+        ];
+
+        # ユーザ情報をDBから取得
+        $user_info = $this->_dao->getFetch($sql, $data);
+
+        # データがなければ
+        if ( count($user_info) === 1 ) {
+            # falseを返す
+            return (bool)false;
+        }
+
+        # パスワードをチェックする
+        if ( password_verify($user_info['password'], $password) !== true ) {
+            return (bool)false;
+        }
+
+        # 返却
+        return (array)$user_info;
+
+    }
+
+
+
+    /**
+     * ユーザの権限をDBから取得して返す
+     * @param  int    $user_id  ユーザIDを指定
+     * @return int
+     */
+    public function getUserPermission(int $user_id) {
+        # DBからユーザ情報を検索するSQL
+        $sql = 'SELECT
+                    permission
+                FROM
+                    user
+                WHERE
+                    user_id = :user_id
+        ';
+
+        # プレースホルダーに当てはめる用
+        $data = [
+            'user_id' => $user_id
+        ];
+
+        # DBからユーザ情報を取得
+        $permission = $this->_dao->getFetch($sql, $data);
+
+        # ユーザIDが存在していなかったらNULLが返ってきているので、NULLの場合は
+        if ( $permission[0] === NULL ) {
+            # 0を返却
+            return 0;
+        }
+
+        # データが存在していれば、int型でパーミッションを返す
+        return (int)$permission['permission'];
+    }
+
+}
