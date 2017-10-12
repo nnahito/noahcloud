6eb305464f6583721f2d1821ba51bf47ae0a34cb
水波形 <suihakei@gmail.com>
2017-10-13 02:19:06 +0900

    アイコンファイルのインクルード

--- include/view/template/include.php
+++ include/view/template/include.php
@@ -1,5 +1,6 @@
 <!-- CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 
 <!-- JavaScript -->
