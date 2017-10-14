3d4ff064bb046000e64ee32ce1b87578b6067ece
Nな人 <nnahiko@gmail.com>
2017-10-15 02:47:56 +0900

    エラーのみを表示させるビュー作成

---
+++ include/view/errorView.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>NOAH CLOUD</title>
+
+    <meta charset="utf-8">
+
+    <!-- CSSとJSを読み込む -->
+    <?php include_once('../include/view/template/include.php'); ?>
+
+</head>
+
+<body>
+
+    <!-- ヘッダー -->
+    <?php include_once('../include/view/template/header.php'); ?>
+
+
+
+    <div class="row">
+
+        <!-- エラー -->
+        <div class="col s8 offset-s2 red accent-1 red-text text-darken-4" style="border: 1px solid #b71c1c; border-radius: 3px; margin-top: 5px; padding: 5px;">
+
+            <ul>
+
+                <?php
+                foreach ($this->errorMsgs as $key => $value) {
+                ?>
+                <li><?= $value ?></li>
+                <?php
+                }
+                ?>
+
+            </ul>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
