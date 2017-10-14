3672bd3481fa0cc47a8dd2e7690bf0b10af3e692
Nな人 <nnahiko@gmail.com>
2017-10-15 02:48:40 +0900

    共通CSSファイルを読み込むように共通ヘッダに追記

--- include/view/template/include.php
+++ include/view/template/include.php
@@ -2,6 +2,8 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
+<link href="/css/main.css" rel="stylesheet">
+
 
 <!-- JavaScript -->
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
